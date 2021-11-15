62733858b7ef7a245968bd89b7220544310881c7
Dmytro  Lytvynenko <writelytvynenkodmytro@gmail.com>
2021-11-15 08:45:33 +0000

    Intro/solution

--- project/coder.h
+++ project/coder.h
@@ -14,34 +14,43 @@
 #ifndef CODER_H
 #define CODER_H
 
+#include <iostream>
+#include <limits>
+
+#include "cstring"
+
 /*
  * This function was pre-compiled and is provided as a part of the
  * static library.
  *
  */
-void encode( char* buf, int size );
+void encode(char* buf, int size);
 
-#pragma pack(push, 1)
+//#pragma pack(push, 1)
 
 /*
  * Coder class header.
  *
  */
-class Coder
-{
-public:
-	void set( const char* buf, int size );
-	char* buf() const;
-	int size() const;
+class Coder {
+ public:
+  Coder();
+  Coder(const Coder& source_coder);
+  Coder& operator=(const Coder& source_coder);
+  void set(const char* buf, int size);
+  char* buf() const;
+  int size() const;
 
-	void encode();
-	void decode();
+  void encode();
+  void decode();
 
-private:
-	char* m_buf;
-	int m_size;
+  ~Coder();
+
+ private:
+  char* m_buf;
+  int m_size;
 };
 
-#pragma pack(pop)
+//#pragma pack(pop)
 
-#endif // CODER_H
+#endif  // CODER_H
